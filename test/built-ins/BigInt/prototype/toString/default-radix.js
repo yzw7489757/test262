82031c6cf5cafd2ba8921fd0412ba36c58291d15
Rick Waldron <waldron.rick@gmail.com>
2018-04-18 16:28:26 -0400

    test BigInt toString with default radix followup

--- test/built-ins/BigInt/prototype/toString/default-radix.js
+++ test/built-ins/BigInt/prototype/toString/default-radix.js
@@ -4,16 +4,28 @@
 /*---
 esid: sec-bigint.prototype.tostring
 description: toString with default radix
+info: |
+  BigInt.prototype.toString ( [ radix ] )
+
+  ...
+  2. If radix is not present, let radixNumber be 10.
+  3. Else if radix is undefined, let radixNumber be 10.
+  ...
+  6. If radixNumber = 10, return ! ToString(x).
+  ...
 features: [BigInt]
 ---*/
 
-assert.sameValue((-100n).toString(), "-100", "(-100n).toString() === '-100'");
-assert.sameValue((0n).toString(), "0", "(0n).toString() === '0'");
-assert.sameValue((100n).toString(), "100", "(100n).toString() === '100'");
+assert.sameValue(-100n.toString(), "-100", "-100n.toString() === '-100'");
+assert.sameValue(0n.toString(), "0", "0n.toString() === '0'");
+assert.sameValue(100n.toString(), "100", "100n.toString() === '100'");
+assert.sameValue(-100n.toString(), "-100", "-100n.toString() === '-100'");
+assert.sameValue(0n.toString(), "0", "0n.toString() === '0'");
+assert.sameValue(100n.toString(), "100", "100n.toString() === '100'");
 
-assert.sameValue((-100n).toString(undefined), "-100",
-                 "(-100n).toString(undefined) === '-100'");
-assert.sameValue((0n).toString(undefined), "0",
-                 "(0n).toString(undefined) === '0'");
-assert.sameValue((100n).toString(undefined), "100",
-                 "(100n).toString(undefined) === '100'");
+assert.sameValue(-100n.toString(undefined), "-100",
+                 "-100n.toString(undefined) === '-100'");
+assert.sameValue(0n.toString(undefined), "0",
+                 "0n.toString(undefined) === '0'");
+assert.sameValue(100n.toString(undefined), "100",
+                 "100n.toString(undefined) === '100'");
