d2bc80d53f253efd8d8337c88b3efcc5d0577f33
Leo Balter <leonardo.balter@gmail.com>
2018-12-21 15:04:06 -0200

    add minimal syntax test for class

---
+++ test/language/statements/class/_syntax.js
@@ -0,0 +1,19 @@
+// Copyright (C) 2018 Leo Balter. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+description: Minimal valid syntax for Classes
+esid: sec-class-definitions
+info: |
+  ClassDeclaration:
+    class BindingIdentifier ClassTail
+
+  ClassExpression:
+    class BindingIdentifier_opt ClassTail
+
+  ClassTail:
+    ClassHeritage_opt { ClassBody_opt }
+features: [class]
+---*/
+
+class C {}
